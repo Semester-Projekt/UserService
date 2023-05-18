c8e3d3d9c219214d95bb07b3e6c92788cefb84c0
danieldawsonjensen <dani540c@gmail.com>
2023-05-18 12:23:06 +0200

    main igen

--- UserServiceApi/Model/UserRepository.cs
+++ UserServiceApi/Model/UserRepository.cs
@@ -16,43 +16,20 @@ namespace Model
         {
             string connectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING"); // mongo conn string miljøvariabel
             var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("User"); // vores database
-            _user = database.GetCollection<User>("Users");
+            var database = client.GetDatabase("Auctionhouse"); // vores database
+            _user = database.GetCollection<User>("User");
         }
 
 
-
-        //GET
         public async Task<User> FindUserByUsernameAndPassword(string userName, string userPassword)
         {
             var filter = Builders<User>.Filter.Eq("UserName", userName) & Builders<User>.Filter.Eq("UserPassword", userPassword);
             return await _user.Find(filter).FirstOrDefaultAsync();
         }
 
-        public async Task<User> GetUser(int id)
-        {
-            var filter = Builders<User>.Filter.Eq("UserId", id);
-            return await _user.Find(filter).FirstOrDefaultAsync();
-        }
-
-
-
-
-
-        //POST
         public void AddNewUser(User user)
         {
             _user.InsertOne(user);
         }
-
-
-
-
-        //DELETE
-        
-
-
-
-        //PUT
     }
 }
