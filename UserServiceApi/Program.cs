aa31500bef59d0cfa6853a02ffaa9568cb769f46
danieldawsonjensen <dani540c@gmail.com>
2023-05-18 12:20:41 +0200

    main

--- UserServiceApi/Program.cs
+++ UserServiceApi/Program.cs
@@ -1,25 +1,84 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
+using Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using NLog;
+using NLog.Web;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var logger =
+NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try // try/catch/finally fra m10.01 opgave b step 4
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    var builder = WebApplication.CreateBuilder(args);
+
+
+    string mySecret = Environment.GetEnvironmentVariable("Secret") ?? "none";
+    string myIssuer = Environment.GetEnvironmentVariable("Issuer") ?? "none";
+    builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = myIssuer,
+            ValidAudience = "http://localhost",
+            IssuerSigningKey =
+         new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
+        };
+    });
+
+    // Add services to the container.
+    builder.Services.AddSingleton<UserRepository>();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+
+    // builder.Logging.ClearProviders(); //Opgave 10.01B step 3 DENNE LINJE FUCKER NOGET UP ??????
+
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
 
-app.UseHttpsRedirection();
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    NLog.LogManager.Shutdown();
+}
