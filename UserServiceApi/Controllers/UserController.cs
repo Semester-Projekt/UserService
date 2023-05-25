913d1e2b1c80c73a4f0ccc0dd7a625eafd5ea6d3
danieldawsonjensen <dani540c@gmail.com>
2023-05-25 17:35:37 +0200

    fix version

--- UserServiceApi/Controllers/UserController.cs
+++ UserServiceApi/Controllers/UserController.cs
@@ -66,7 +66,7 @@ public class UserController : ControllerBase
             Description = description
         };
 
-        return Ok("THIS IS A TEST, THIS IMAGE SHOULD SHOW: MAIN");
+        return Ok(versionInfo);
     }
 
 
